b479e5c5570ef2068685905b35e6931a48827ff7
MateenAbbasi603 <mateenabbasi603@gmail.com>
2025-04-09 03:44:54 +0500

    Add OrderService entity to the database context with relationships to Order and Service; update seed data for Super Admin password

---
+++ fyp-motomate/Migrations/20250408224356_AddOrderServices.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fyp_motomate.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderServices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderServices",
+                columns: table => new
+                {
+                    OrderServiceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ServiceId = table.Column<int>(type: "int", nullable: false),
+                    AddedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderServices", x => x.OrderServiceId);
+                    table.ForeignKey(
+                        name: "FK_OrderServices_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderServices_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "ServiceId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$uinnps6mTDbtz9jU2yEzdusjiGWsk1VnFw13IwHsKUvn1zR7QbxyW");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderServices_OrderId",
+                table: "OrderServices",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderServices_ServiceId",
+                table: "OrderServices",
+                column: "ServiceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderServices");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$SVEuvE8ghC7tJ7o/lMTkUulDT/hyaSwFQ2Uavbkrd9UxLVo8Hus2C");
+        }
+    }
+}
